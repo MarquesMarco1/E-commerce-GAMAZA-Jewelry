2ff39a3de6f2426681d099b4f66f5f9642298837
guillaume <guillaume.reboullet@epitech.eu>
2024-07-17 17:10:40 +0200

    test gitignore

---
+++ server/src/Middleware/Admin.php
@@ -0,0 +1,12 @@
+<?php
+
+// namespace App\Middleware;
+// use App\Repository
+
+// class Admin implements Middleware
+// {
+//     public function wrap(Driver $driver): Driver
+//     {
+//         return new PreventRootConnectionDriver($driver);
+//     }
+// }
