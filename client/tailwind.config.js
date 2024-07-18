e83be4d20da360b912c40460c95870ecbd8bf069
guillaume <guillaume.reboullet@epitech.eu>
2024-07-18 18:01:19 +0200

    Pop Up Register v1

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -13,7 +13,11 @@ module.exports = {
         "light-purple": "#CD92F2",
         "dark-purple": "#7B16B9",
         "black": "#0C0910",
-        "white": "#DEDEE0",
+        "white": "#F5F5F5",
+      }, 
+      backgroundImage: {
+        'login': "url('./assets/login.jpg')",
+        'register': "url('./assets/register.jpg')"
       }
     },
   },
