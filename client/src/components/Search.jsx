86776aaf9216f9755aaaf8c55de99e7f7392ed59
guillaume <guillaume.reboullet@epitech.eu>
2024-07-25 14:38:22 +0200

    Feat (search.jsx): logic search result

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -4,81 +4,82 @@ import { useEffect, useState } from "react";
 
 export default function Search() {
   const [product, setProduct] = useState([]);
-  const [categories, setCategories] =  useState([]);
+  const [categories, setCategories] = useState([]);
   const [productName, setProductName] = useState('');
   const [categoryName, setCategoryName] = useState('All Categories');
-  const [searchResults, setSearchResults]= useState([]);
+  const [searchResults, setSearchResults] = useState([]);
   const [displaySearchResults, setDisplaySearchResults] = useState(false);
-    const [error, setError] = useState('');
+  const [error, setError] = useState('');
 
-    useEffect(() => {
-        const fetchProduct = async () => {
-          try {
-            const response = await fetch(`${localhost}/api/search`);
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await fetch(`${localhost}/api/search`);
 
-            if (!response.ok) {
-              throw new Error(`Failed to fetch product: ${response.status} ${response.statusText}`);
-            }
-
-            if (response.ok) {
-            const data = await response.json();
-            console.log(data);
-            if (data.product && data.product.length > 0) {
-              setProduct(data.product);
-              setCategories(data.category)
-            } else {
-              setError(new Error("Product not found"));
-            }
-          } else {
-            setError(new Error("Failed to fetch product"));
-          }
-        } catch (err) {
-          setError(err);
+        if (!response.ok) {
+          throw new Error(`Failed to fetch product: ${response.status} ${response.statusText}`);
         }
-      };
+
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data);
+          if (data.product && data.product.length > 0) {
+            setProduct(data.product);
+            setCategories(data.category)
+          } else {
+            setError(new Error("Product not found"));
+          }
+        } else {
+          setError(new Error("Failed to fetch product"));
+        }
+      } catch (err) {
+        setError(err);
+      }
+    };
 
     fetchProduct();
   }, []);
-      
-    const handleSearch = async (e) => {
-        e.preventDefault();
-        sortResults();
-    };
 
-    const sortResults = () => {
-      console.log('produits:', product);
-      console.log('nom du produit:', productName);
-      console.log('category:', categories);
-      console.log('nom de la categorie:', categoryName);
-      
-      // Handle Logic //
-      switch(categoryName && product) {
-        default: 'No products found'
-        break;
-        case categoryName == 'All Categories' && productName == '':
-          let listNames = [];
-          product.map((elem) => {
-            console.log('elem:', elem);
-            return listNames;
-          })
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    sortResults();
+    console.log(searchResults)
+  };
+
+  const sortResults = () => {
+    // Handle Logic //
+    let list = [];
+    
+    if (categoryName == 'All Categories' && productName == '') {
+      product.map((elem) => {
+        list.push(elem)
+      })
+    } else {
+      product.map((elem) => {
+        let startWith = elem.name.toLocaleLowerCase().toString().includes(productName.toLocaleLowerCase().toString())
+        if(startWith && elem.category.name == categoryName){
+          list.push(elem)
         }
+      })
     }
-    // Gestion d'erreurs //
-
-    if (error)
-      return (
-        <div className="text-center py-4 text-red-500">
-          Error: {error.message}
-        </div>
-      );
-    if (!product) return <div className="text-center py-4">No product found</div>;
-
-
+    setSearchResults(list);
+  }
+  
+  // Gestion d'erreurs //
+  if (error)
     return (
-      <>
+      <div className="text-center py-4 text-red-500">
+        Error: {error.message}
+      </div>
+    );
+  if (!product) return <div className="text-center py-4">No product found</div>;
+
+
+  return (
+    <>
       <Header />
-        <form onSubmit={handleSearch}
-              className="flex flex-col md:flex-row gap-3">
+      <form onSubmit={handleSearch}
+        className="flex flex-col md:flex-row gap-3">
         <div className="flex">
 
           <input
@@ -87,42 +88,42 @@ export default function Search() {
             value={productName}
             onChange={(e) => setProductName(e.target.value)}
             className="w-full md:w-80 px-3 font-primary h-10 rounded-l border-2 border-gold focus:outline-none focus:border-gold"
-            />
+          />
 
           <button type="submit"
-              className="rounded-r p-3 px-2 md:px-3 py-0 md:py-1 bg-light-purple text-xl font-bold text-black rounded-lg hover:bg-gold font-primary">
-              Search
+            className="rounded-r p-3 px-2 md:px-3 py-0 md:py-1 bg-light-purple text-xl font-bold text-black rounded-lg hover:bg-gold font-primary">
+            Search
           </button>
 
         </div>
 
-          <select
-            value={categoryName}
-            className="w-full h-10 border-2 border-gold focus:outline-none focus:border-gold text-gold rounded px-2 md:px-3 py-0 md:py-1 tracking-wider"
-            onChange={(e) => setCategoryName(e.target.value)}
-          >
-            <option className="text-gold font-primary">All Categories</option>
+        <select
+          value={categoryName}
+          className="w-full h-10 border-2 border-gold focus:outline-none focus:border-gold text-gold rounded px-2 md:px-3 py-0 md:py-1 tracking-wider"
+          onChange={(e) => setCategoryName(e.target.value)}
+        >
+          <option className="text-gold font-primary">All Categories</option>
 
-            {categories.length > 0 && categories.map((elem) => (
+          {categories.length > 0 && categories.map((elem) => (
             <option key={elem.id} value={elem.name}>{elem.name}</option>
-            ))}
+          ))}
 
-          </select>
-        </form>
+        </select>
+      </form>
 
-        <div>
-          {searchResults.length > 0 ? (
-              searchResults.map((result) => (
-                <div key={result.id}>
-                  <h3 className="font-primary text-gold">{result.name}</h3>
-                  <p>{result.description}</p>
-                </div>
-            ))
-          ) : (
-            <p>No results found.</p>
-          )}
-        </div>
-        </>
-    )
-  }
+      <div>
+        {searchResults.length > 0 ? (
+          searchResults.map((result) => (
+            <div key={result.id}>
+              <h3 className="font-primary text-gold">{result.name}</h3>
+              <p>{result.description}</p>
+            </div>
+          ))
+        ) : (
+          <p>No results found.</p>
+        )}
+      </div>
+    </>
+  )
+}
 
