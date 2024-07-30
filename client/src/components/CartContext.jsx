e34d92a4e77f5432381a419dbaed01925fb258f7
Marco Marques <marco.marques@epitech.eu>
2024-07-30 19:14:24 +0200

    continuité de ce que j'ai pu faire hier avec l'ajout de produits au panier

--- client/src/components/CartContext.jsx
+++ client/src/components/CartContext.jsx
@@ -1,17 +1,37 @@
 import React, { createContext, useState } from 'react';
+import localhost from "../config";
 
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState([]);
 
-  const addToCart = (product, quantity, selectedSize, selectedColor) => {
-    setCart((prevCart) => [...prevCart, { product, quantity, selectedSize, selectedColor }]);
-  };
+    const addToCart = async (product, quantity, selectedSize, selectedColor) => {
+        const cartItem = { productId: product.id, quantity, selectedSize, selectedColor };
+        
+    try {
+        const response = await fetch(`${localhost}/api/cart`, {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(cartItem),
+        });
 
-  return (
-    <CartContext.Provider value={{ cart, addToCart }}>
-      {children}
-    </CartContext.Provider>
-  );
+        if (!response.ok) {
+            throw new Error('Failed to add item to cart');
+        }
+
+        const updatedCart = await response.json();
+        setCart(updatedCart);
+        } catch (error) {
+        console.error('Error adding item to cart:', error);
+        }
+    };
+
+    return (
+        <CartContext.Provider value={{ cart, addToCart }}>
+        {children}
+        </CartContext.Provider>
+    );
 };
