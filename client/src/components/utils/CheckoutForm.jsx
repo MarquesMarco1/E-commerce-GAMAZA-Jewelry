a5af8f1aa7b5125a2655c441570a18afb0351e34
guillaume <guillaume.reboullet@epitech.eu>
2024-08-02 18:41:19 +0200

    Save: Integration related product in stripe form

--- client/src/components/utils/CheckoutForm.jsx
+++ client/src/components/utils/CheckoutForm.jsx
@@ -1,16 +1,28 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import localhost from '../../config';
-
+import { useCart } from "../../CartContext";
 import {
     EmbeddedCheckoutProvider,
     EmbeddedCheckout
 } from '@stripe/react-stripe-js';
 
-export default function CheckoutForm (props) {
+export default function CheckoutForm(props) {
+    const { state: cart, dispatch } = useCart([]);
+    const [stripeData, setStripeData] = useState([])
     const fetchClientSecret = useCallback(async () => {
         // Create a Checkout Session
+        cart.map((item)=>{
+            // const formData = {
+            //     price: item.produ
+            // }
+            console.log(item)
+        })
         const res = await fetch(`${localhost}/api/checkout`, {
             method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            // body: JSON.stringify({ formData }),
         });
         const data = await res.json();
         return data.clientSecret;
--- server/migrations/Version20240802162128.php
+++ server/migrations/Version20240802162128.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240802153943 extends AbstractMigration
+final class Version20240802162128 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,18 @@ final class Version20240802153943 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE size');
         $this->addSql('ALTER TABLE product ADD key_stripe VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE review ADD product_id INT NOT NULL');
+        $this->addSql('ALTER TABLE review ADD CONSTRAINT FK_794381C64584665A FOREIGN KEY (product_id) REFERENCES product (id)');
+        $this->addSql('CREATE INDEX IDX_794381C64584665A ON review (product_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE size (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, last_update DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('ALTER TABLE product DROP key_stripe');
+        $this->addSql('ALTER TABLE review DROP FOREIGN KEY FK_794381C64584665A');
+        $this->addSql('DROP INDEX IDX_794381C64584665A ON review');
+        $this->addSql('ALTER TABLE review DROP product_id');
     }
 }
