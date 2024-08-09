8a05d982025c28c404c6ccdc42afb692a8a9ec1c
Zoe <zoe.mahler@epitech.eu>
2024-08-09 12:30:31 +0200

    feat : switch logic front-end only

--- client/src/components/admin/dashboard/Shipping.jsx
+++ client/src/components/admin/dashboard/Shipping.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useContext } from "react";
 import localhost from "../../../config";
 import { useTranslation } from "react-i18next";
-
+import switchIcon from '../../../assets/switchList.svg';
 
 //////////////////
 //  Components  //
@@ -21,6 +21,7 @@ export default function ManageShipping() {
 
   const [whitelist, setWhitelist] = useState([]);
   const [blacklist, setBlacklist] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState(null);
 
     ///////////////////////////////////////////////
   //  Fetch Whitelist, Blacklist, language, shipping  //
@@ -29,13 +30,10 @@ export default function ManageShipping() {
   async function getCountries() {
     try {
       const res = await fetch(`${localhost}/api/shippingCountry`);
-      
       if(!res.ok) {
         throw new Error(`HTTP error : ${res.status}`);
       }
-
-      const data = await res.json();
-      console.log(data);
+      const data = await res.json();  
       setWhitelist(data.whitelist);
       setBlacklist(data.blacklist);
     } catch (error) {
@@ -50,6 +48,25 @@ export default function ManageShipping() {
   const sortedWhitelist = whitelist.slice().sort((a, b) => a.name.localeCompare(b.name));
   const sortedBlacklist = blacklist.slice().sort((a, b) => a.name.localeCompare(b.name));
 
+  const switchLists = () => {
+    if(!selectedCountry) return;
+
+    const whitelistCountries = whitelist.find(country => country.name === selectedCountry);
+    const blacklistCountries = blacklist.find(country => country.name === selectedCountry);
+
+    if(whitelistCountries) {
+      setWhitelist(whitelist.filter(country => country.name !== selectedCountry));
+      setBlacklist([...blacklist, whitelistCountries]);
+    }else if (blacklistCountries) {
+      setBlacklist(blacklist.filter(country => country.name !== selectedCountry));
+      setWhitelist([...whitelist, blacklistCountries]);
+    }
+    setSelectedCountry(null);
+  };
+
+  const handleSelect = (e) => {
+    setSelectedCountry(e.target.value);
+  };
 
   return (
     <>
@@ -63,7 +80,12 @@ export default function ManageShipping() {
                 <h2 className="text-center text-xl mb-4 mt-4 text-black dark:text-gold font-primary">
                 {t("manageShipping.whitelist")}
               </h2>
-              <select className="w-full p-2 border border-gold rounded dark:bg-dark-mode-purple dark:border-gold">
+              <select 
+                className="w-full p-2 border border-gold rounded dark:bg-dark-mode-purple dark:border-gold"
+                onChange={handleSelect}
+                value={selectedCountry || ""}
+                >
+                  <option value="" disabled>Select a country</option>
                 {sortedWhitelist.map((country) => (
                 <option key={country.id} value={country.name}>
                   {country.name}
@@ -71,11 +93,28 @@ export default function ManageShipping() {
                 ))}
               </select>
             </div>
+
+            <button
+            onClick={switchLists}
+            className="bg-transparent border-none cursor-pointer"
+          >
+            <img
+              src={switchIcon}
+              className="w-10 h-10 md:w-20 md:h-20"
+              alt="double arrow icon to switch countries between whitelist and blacklist"
+            />
+          </button>
+
             <div className="dropdown">
               <h2 className="text-center text-xl mb-4 mt-4 text-black dark:text-gold font-primary">
                 {t("manageShipping.blacklist")}
               </h2>
-              <select className="w-full p-2 border border-gold rounded dark:bg-dark-mode-purple dark:border-gold">
+              <select 
+                className="w-full p-2 border border-gold rounded dark:bg-dark-mode-purple dark:border-gold hover:bg-light-purple dark:hover:bg-dark-mode-light-purple"
+                onChange={handleSelect}
+                value={selectedCountry || ""}
+              >
+                <option value="" disabled>Select a country</option>
                 {sortedBlacklist.map((country) => (
                   <option key={country.id} value={country.name}>
                     {country.name}
@@ -83,6 +122,8 @@ export default function ManageShipping() {
                 ))}
               </select>
             </div>
+          </div>
+          <div className="text-center mt-4">
           </div>
           <h1 className="text-center text-2xl mb-4 mt-4 text-gold">
             {t("manageShipping.thresholdsTitle")}
