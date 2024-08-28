dd792f50d06074c8418d24af279a680712d9f7cd
Liilice <alice.zheng@epitech.eu>
2024-08-28 18:52:30 +0200

    Version sprint 5

--- client/src/components/utils/CheckoutForm.jsx
+++ client/src/components/utils/CheckoutForm.jsx
@@ -1,71 +1,69 @@
 import React, { useCallback, useEffect, useState } from "react";
-import localhost from '../../config';
+import localhost from "../../config";
 import { useCart } from "../../CartContext";
 import { useLocation } from "react-router-dom";
 import {
-    EmbeddedCheckoutProvider,
-    EmbeddedCheckout
-} from '@stripe/react-stripe-js';
+  EmbeddedCheckoutProvider,
+  EmbeddedCheckout,
+} from "@stripe/react-stripe-js";
 
 export default function CheckoutForm(props) {
-    const { state: cart, dispatch } = useCart([]);
-    const [stripeData, setStripeData] = useState([]);
-    const [clientSecret, setClientSecret] = useState('');
+  const { state: cart, dispatch } = useCart([]);
+  const [stripeData, setStripeData] = useState([]);
+  const [clientSecret, setClientSecret] = useState("");
 
-    const params = useLocation();
-    const adress = params.state.adress;
-    const shipping_amount = params.state.shipping_amount;
-    const shipping_name = params.state.shipping_name;
-    const shipping_estimatedDays = params.state.shipping_estimatedDays;
+  const params = useLocation();
+  const adress = params.state.adress;
+  const shipping_amount = params.state.shipping_amount;
+  const shipping_name = params.state.shipping_name;
+  const shipping_estimatedDays = params.state.shipping_estimatedDays;
 
+  // console.log(`adress => ${params.state.adress} | shipping => ${params.state.shipping}`)
+  useEffect(() => {
+    const newStripeData = cart.map((item) => {
+      return {
+        price: item.product.keyStripe,
+        quantity: item.itemQty,
+        adress: adress,
+        shipping_amount: shipping_amount,
+        shipping_name: shipping_name,
+        shipping_estimatedDays: shipping_estimatedDays,
+      };
+    });
+    setStripeData(newStripeData);
+  }, [cart]);
 
-    // console.log(`adress => ${params.state.adress} | shipping => ${params.state.shipping}`)
-    useEffect(() => {
-        const newStripeData = cart.map((item) => {
-            return {
-                price: item.product.keyStripe,
-                quantity: item.itemQty, 
-                adress: adress,
-                shipping_amount: shipping_amount,
-                shipping_name: shipping_name,
-                shipping_estimatedDays: shipping_estimatedDays
-            };
-        });
-        setStripeData(newStripeData);
-    }, [cart]);
+  const fetchClientSecret = useCallback(async () => {
+    if (stripeData.length === 0) return;
+    const res = await fetch(`${localhost}/api/checkout`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(stripeData),
+    });
 
-    const fetchClientSecret = useCallback(async () => {
-        if (stripeData.length === 0) return;
-            const res = await fetch(`${localhost}/api/checkout`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(stripeData),
-            });
+    const data = await res.json();
 
-            const data = await res.json();
+    // console.log(data);
+    if (data.clientSecret) {
+      setClientSecret(data.clientSecret);
+    }
+  }, [stripeData]);
 
-            console.log(data)
-            if (data.clientSecret) {
-                setClientSecret(data.clientSecret);
-            }
-    }, [stripeData]);
+  useEffect(() => {
+    fetchClientSecret();
+  }, [stripeData, fetchClientSecret]);
 
-    useEffect(() => {
-        fetchClientSecret();
-    }, [stripeData, fetchClientSecret]);
+  const options = { clientSecret };
 
-    const options = { clientSecret };
-
-    return (
-        <div id="checkout">
-            {clientSecret && <EmbeddedCheckoutProvider
-                stripe={props.stripe}
-                options={options}
-            >
-                <EmbeddedCheckout />
-            </EmbeddedCheckoutProvider>}
-        </div>
-    );
+  return (
+    <div id="checkout">
+      {clientSecret && (
+        <EmbeddedCheckoutProvider stripe={props.stripe} options={options}>
+          <EmbeddedCheckout />
+        </EmbeddedCheckoutProvider>
+      )}
+    </div>
+  );
 }
